f45566aef4a09da26563b0b7548b3f8efe49b930
snehalupatil <patilsnehal4575@gmail.com>
2021-05-23 16:03:02 +0530

    Added: Create an Employee Payroll Service

---
+++ main/java/EmployeePayrollData.java
@@ -0,0 +1,17 @@
+public class EmployeePayrollData
+{
+    public int id;
+    public String name;
+    public double salary;
+
+    public EmployeePayrollData(int id,String name,double salary){
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+
+    public  String toString(){
+        return "EmployeePayRollData[id=" + id + ",name=" + name + ",salary=" + salary + "]";
+    }
+}
---
+++ main/java/EmployeePayrollService.java
@@ -0,0 +1,36 @@
+import jdk.dynalink.linker.LinkerServices;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmployeePayrollService
+{
+    private List<EmployeePayrollData> employeePayrollList;
+
+    public EmployeePayrollService(List<EmployeePayrollData>employeePayrollList){
+        this.employeePayrollList = employeePayrollList;
+    }
+
+    public static void main(String[] args){
+        ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+        Scanner consoleInputReader = new Scanner(System.in);
+        employeePayrollService.readEmployeePayRollData(consoleInputReader);
+        employeePayrollService.writeEmployeePayRollData();
+    }
+
+    private void readEmployeePayRollData(Scanner consoleInputReadr){
+    System.out.println("Enter Employee Id:");
+    int id = consoleInputReadr.nextInt();
+    System.out.println("Enter Employee Name:");
+    String name =consoleInputReadr.next();
+    System.out.println("Enter Employee Salary:");
+    double salary = consoleInputReadr.nextDouble();
+
+    }
+
+    private void writeEmployeePayRollData(){
+        System.out.println(employeePayrollList);
+    }
+}
